71cf2c8eca60b803b8c52ba29c1d7f0a50d802ff
Emilio Schaad <66437800+DarkCat11@users.noreply.github.com>
2024-05-11 20:37:36 -0600

    Update NodoArbol.java
    Leer los datos e inicializarlo

--- src/proyectofinalprogramacioniii/NodoArbol.java
+++ src/proyectofinalprogramacioniii/NodoArbol.java
@@ -9,5 +9,22 @@ package proyectofinalprogramacioniii;
  * @author PC
  */
 public class NodoArbol {
+    int ID;
+    String DPI;
+    String NOMBRE;
     
+    NodoArbol RamaDerecha, RamaIzquierda;
+    
+    public NodoArbol(int id, String dpi, String nombre)
+    {
+        this.ID = id;
+        this.DPI = dpi;
+        this.NOMBRE = nombre;
+        this.RamaDerecha = null;
+        this.RamaIzquierda = null;
+    }
+    
+    public String toString(){
+        return "Id de pasajero: " + ID + " Documento personal de identificacion: " + DPI + "Nombre del pasajero: " + NOMBRE;
+    }
 }
