c2a9aaa0ce870ca1d43869118fa65b2a487aa491
Emilio Schaad <66437800+DarkCat11@users.noreply.github.com>
2024-05-13 13:01:59 -0600

    Creacion funcion insertar

--- src/proyectofinalprogramacioniii/ArbolBusqueda.java
+++ src/proyectofinalprogramacioniii/ArbolBusqueda.java
@@ -9,9 +9,46 @@ package proyectofinalprogramacioniii;
  * @author PC
  */
 public class ArbolBusqueda {
+
     NodoArbol raiz;
-    
-    public ArbolBusqueda(){
+
+    public ArbolBusqueda() {
         raiz = null;
+
+    }
+
+    public void InsetarDatos(int id, String dpi, String nombre) {
+        NodoArbol newTree= new NodoArbol(id, dpi, nombre);
+        if(raiz == null)
+        {
+            raiz = newTree;
+        }else{
+            NodoArbol ArbolAuxiliar = raiz;
+            NodoArbol Raiz;
+            
+            while(true)
+            {
+                Raiz = ArbolAuxiliar;
+                if(id < ArbolAuxiliar.ID)
+                {
+                    ArbolAuxiliar = ArbolAuxiliar.RamaIzquierda;
+                    if(ArbolAuxiliar == null)
+                    {
+                        Raiz.RamaIzquierda = newTree;
+                        return;
+                    }
+                }else
+                {
+                    ArbolAuxiliar = ArbolAuxiliar.RamaDerecha;
+                    if(ArbolAuxiliar == null)
+                    {
+                        Raiz.RamaDerecha = newTree;
+                        return;
+                    }
+                }
+            }
+        }
+        
+
     }
 }
