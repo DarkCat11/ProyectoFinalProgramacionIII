2e09495a494c27e913c7961b444288659be498e9
Emilio Schaad <66437800+DarkCat11@users.noreply.github.com>
2024-05-14 20:14:26 -0600

    Archivos

---
+++ src/proyectofinalprogramacioniii/ManejoDeArchivos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinalprogramacioniii;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+/**
+ *
+ * @author PC
+ */
+public class ManejoDeArchivos {
+    
+    void GenerarArchivo(String Pasajeros, int ID, String DPI, String nombre){
+        
+        try{
+            FileWriter Usuarios = new FileWriter(Pasajeros, true);
+            BufferedWriter bufferedwriter = new BufferedWriter(Usuarios);
+            PrintWriter printwriter = new PrintWriter(bufferedwriter);
+            printwriter.println(ID + "," + DPI + "," + nombre);
+            
+            printwriter.flush();
+            printwriter.close();
+            
+            
+        }catch(IOException e)
+        {
+            System.out.print(e);
+        }
+    }
+    
+     int ID()
+    {
+        Random Numero = new Random();
+        int NumeroRandom = Numero.nextInt((99999 + 1) + 1);
+        return NumeroRandom;
+    }
+     
+     void eliminarArchivo(String NombreArchivo)
+     {
+         
+     }
+}
--- src/proyectofinalprogramacioniii/ProyectoFinalProgramacionIII.java
+++ src/proyectofinalprogramacioniii/ProyectoFinalProgramacionIII.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyectofinalprogramacioniii;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -14,7 +16,72 @@ public class ProyectoFinalProgramacionIII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner cin = new Scanner (System.in);
+        ManejoDeArchivos CSV = new ManejoDeArchivos();
+        
+        String dpi = null;
+        String nombre = null;
+        int id = 0;
+        boolean flag = false;
+       
+         IngresarDatosCSV(id, dpi, nombre, flag);
+            
+            
     }
     
+    public static boolean ValidarNumeros(String numero)
+    {
+        return numero.matches("[0-9]{13,13}");
+    }
+   
+    public static boolean ValidarLetras(String texto)
+    {
+        return texto.matches("[a-zA-Z]*");
+    }
+    
+    public static void IngresarDatosCSV(int id, String dpi, String nombre, boolean flag)
+    {
+        Scanner cin = new Scanner (System.in);
+        ManejoDeArchivos CSV = new ManejoDeArchivos();
+       
+        
+        for(int i = 0; i < 4 ; i ++)
+        {
+           id = CSV.ID();
+           System.out.print("\nSu ID: " + id);
+           
+           System.out.print("\nIngrese su dpi: ");
+           dpi = cin.next();
+           
+           while(flag == false)
+           {
+               if(!ValidarNumeros(dpi))
+               {
+               System.out.print("\nDPI NO VALIDO, VUELVA A INGRESAR: ");
+               dpi = cin.next();
+               }else{
+                   flag = true;
+               }
+           }
+           
+           flag = false;
+           
+           System.out.print("\nIngrese su nombre: "); 
+           nombre = cin.next();
+           
+           
+           while(flag == false)
+           {
+               if(!ValidarLetras(nombre))
+               {
+               System.out.print("\nINGRESO SU NOMBRE INCORRECTAMENTE, VUELVA A INGRESAR: ");
+               nombre = cin.next();
+               }else{
+                   flag = true;
+               }
+           }
+           flag = false;
+           CSV.GenerarArchivo("Pasajeros.csv", id, dpi, nombre);
+        }
+    }
 }
